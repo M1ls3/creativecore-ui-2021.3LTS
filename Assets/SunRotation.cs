51b70fa6d274b2efe25b3596172a3d697b62aabc
M1ls3 <sofarua@gmail.com>
2024-04-22 01:37:52 +0300

    Update SunRotation.cs

--- Assets/SunRotation.cs
+++ Assets/SunRotation.cs
@@ -19,14 +19,11 @@ public class SunRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // You can remove this line from Update, as it's better to add the listener once in Start
+        
     }
 
     private void OnSliderValueChanged(float value)
     {
-        // Calculate the new rotation angle based on the slider value
-        float rotationAngle = value;
-
         // Apply the rotation, combining it with the start angle to maintain the initial orientation
         sunTransform.rotation = Quaternion.Euler(value, 291.261f, 12.882f);
     }
